5e7f94937bfd8ddf06a7d2e5c4ae198d23352a15
ironhead <iron_head@icloud.com>
2022-05-22 05:35:46 +0800

    :construction: (homework2): implement basic agglomerative clustering

--- src/homework2/AgglomerativeClustering.py
+++ src/homework2/AgglomerativeClustering.py
@@ -1,8 +1,6 @@
+from cmath import inf
 import numpy as np
 from typing import Callable, Optional,Union
-from sklearn.cluster import affinity_propagation
-
-from torch import affine_grid_generator, narrow, per_channel_affine
 
 class AgglomerativeClustering(object):
     def __init__(self, 
@@ -11,15 +9,19 @@ class AgglomerativeClustering(object):
                 affinity:Union[str,Callable] = 'euclidean',
                 memory = None,
                 connectivity=None, 
-                compute_full_tree='auto', 
+                compute_full_tree=None, 
                 linkage:str='ward', 
                 distance_threshold=None
                 ) -> None:
         self.n_clusters = n_clusters
         self.affinity=affinity
         self.memory=memory
+        if memory is not None:
+            raise RuntimeError('unsupported argument memory')
         self.connectivity=connectivity
         self.compute_full_tree = compute_full_tree
+        if compute_full_tree is not None:
+            raise RuntimeError('unsupported argument compute_full_tree')
         self.linkage=linkage
         self.distance_threshold=distance_threshold
     
@@ -31,7 +33,9 @@ class AgglomerativeClustering(object):
             raise RuntimeError('do not know when the clustering ends')
         if self.linkage == 'ward' and self.affinity != 'euclidean':
             raise RuntimeError('only euclidean distance can be used when the linkage is ward')
-
+        n_samples = len(X)
+        self.children_ = np.zeros((n_samples-1,2),dtype=np.int32)
+        self.labels_ = np.zeros((n_samples),dtype=np.int32)
         if self.n_clusters is not None:
             return self._fit_by_n_clusters(X,y)
         else:
@@ -41,24 +45,136 @@ class AgglomerativeClustering(object):
         n_samples = len(X)
         distance_threshold = self.distance_threshold
         ## initialize 
-        sets = [set(i) for i in range(n_samples)]
-        pass
+        sets = dict([(i,set([i])) for i in range(n_samples)])
+        if self.affinity!='ward':
+            self.connectivity=self._precompute_distance(X)
+        num_branch = n_samples
+        while len(sets)>1:
+            set_min1=None
+            set_min2=None
+            min_distance = inf
+            key_values = [key for key in sets.keys()]
+            for i in range(len(key_values)-1):
+                key_i = key_values[i]
+                for j in range(i+1,len(key_values)):
+                    key_j = key_values[j]
+                    if min_distance > self._distance_between_sets(sets[key_i],sets[key_j]):
+                        set_min1 = key_i
+                        set_min2 = key_j
+                        min_distance = self._distance_between_sets(sets[key_i],sets[key_j])
+            if min_distance > distance_threshold:
+                break
+            sets[num_branch]=sets[set_min1] | sets[set_min2]
+            sets.pop(set_min1)
+            sets.pop(set_min2)
+            self.children_[num_branch-n_samples,0]=set_min1
+            self.children_[num_branch-n_samples,1]=set_min2
+            num_branch+=1
+        class_id = 0
+        self.n_clusters_ = 2*n_samples-num_branch
+        for values in sets.values():
+            for id in values:
+                self.labels_[id]=class_id
+            class_id += 1
+        y=self.labels_
+        return self.labels_
+        
 
     def _fit_by_n_clusters(self,X,y=None):
         n_samples = len(X)
         n_clusters = self.n_clusters
         ## initialize 
-        sets = [set(i) for i in n_clusters:
+        sets = dict([(i,set([i])) for i in range(n_samples)])
+        if self.affinity!='ward':
+            self.connectivity=self._precompute_distance(X)
+        num_branch = n_samples
+        while len(sets)>n_clusters:
+            set_min1=None
+            set_min2=None
+            min_distance = inf
+            key_values = [key for key in sets.keys()]
+            for i in range(len(key_values)-1):
+                key_i = key_values[i]
+                for j in range(i+1,len(key_values)):
+                    key_j = key_values[j]
+                    if min_distance > self._distance_between_sets(sets[key_i],sets[key_j],X):
+                        set_min1 = key_i
+                        set_min2 = key_j
+                        min_distance = self._distance_between_sets(sets[key_i],sets[key_j],X)
+            sets[num_branch]=sets[set_min1] | sets[set_min2]
+            sets.pop(set_min1)
+            sets.pop(set_min2)
+            self.children_[num_branch-n_samples,0]=set_min1
+            self.children_[num_branch-n_samples,1]=set_min2
+            num_branch+=1
+        class_id = 0
+        self.n_clusters_ = n_clusters
+        for values in sets.values():
+            for id in values:
+                self.labels_[id]=class_id
+            class_id += 1
+        y=self.labels_
+        return self.labels_
 
-        pass
+
+    @property
+    def _distance_between_sets(self):
+        def single_link(x:set,y:set, *_):
+            set_distance = inf
+            for a in x:
+                for b in y:
+                    if set_distance > self.connectivity[a,b]:
+                        set_distance = self.connectivity[a,b]
+            return set_distance
+
+        
+        def complete_link(x:set,y:set, *_):
+            set_distance = -inf
+            for a in x:
+                for b in y:
+                    if set_distance < self.connectivity[a,b]:
+                        set_distance = self.connectivity[a,b]
+            return set_distance
+        
+        def average_link(x:set,y:set,*_):
+            sum_distance = 0.0
+            for a in x:
+                for b in y:
+                    sum_distance += self.connectivity[a,b]
+            return sum_distance/(len(x)*len(y))
+
+        def ward_link(x:set,y:set,X ,*_):
+            m_x = np.zeros((X.shape[1]))
+            m_y = np.zeros((X.shape[1]))
+            for a in x:
+                m_x += X[a,:]
+            for b in y:
+                m_y += X[b,:]
+            m_x /= len(x)
+            m_y /= len(y)
+            return len(x)*len(y)*self._distance_function(m_x,m_y)/(len(x)+len(y))
+            
+        distance_map = {
+            'ward': ward_link,
+            'complete': complete_link,
+            'average': average_link,
+            'single': single_link
+        }
+        if self.linkage not in distance_map:
+            raise ValueError('linkage value error')
+        return distance_map[self.linkage]
 
     def _precompute_distance(self,X):
+        if self.affinity == 'precomputed':
+            return self.connectivity
         n_samples = len(X)
         distance_matrix = np.zeros((n_samples,n_samples))
-        
-        pass
+        for i in range(n_samples):
+            for j in range(n_samples):
+                distance_matrix[i,j]=self._distance_function(X[i,:],X[j,:])
+        return distance_matrix
     
-
+    @property
     def _distance_function(self):
         if callable(self.affinity):
             return self.affinity
@@ -68,6 +184,18 @@ class AgglomerativeClustering(object):
                 'l1': lambda x,y: np.linalg.norm(x-y,ord=1),
                 'l2': lambda x,y: np.linalg.norm(x-y,ord=2),
                 'manhattan': lambda x,y: np.linalg.norm(x-y,ord=1),
-                'cosine': lambda x,y: np.dot(x,y)/(np.linalg.norm(x)*(np.linalg.norm(y)))
-                'percomputed'
+                'cosine': lambda x,y: np.dot(x,y)/(np.linalg.norm(x)*(np.linalg.norm(y))),
+                'percomputed': lambda x,y: None
             }
+            if self.affinity in affinity_map:
+                return affinity_map[self.affinity]
+            else:
+                raise ValueError('affinity value error')
+
+if __name__ == '__main__':
+    clustering = AgglomerativeClustering(3,affinity='l1',linkage='single')
+    print(clustering._distance_function)
+    X = np.array([[0, 0, 0], [0, 0, 0], [1, 1, 1], [1, 1, 1],[2, 2, 2], [2, 2, 2]])
+    true_labels = np.array([0, 0, 1, 1, 2, 2])
+    labels = clustering.fit(X)
+    print(labels)
